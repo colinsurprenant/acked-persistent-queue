fe5c08fa562dacb1959e42681d86514ae5812961
Colin Surprenant <colin.surprenant@gmail.com>
2016-06-18 01:40:54 -0400

    moar tests

--- src/test/java/org/logstash/MemoryQueuePageTest.java
+++ src/test/java/org/logstash/MemoryQueuePageTest.java
@@ -12,7 +12,7 @@ public class MemoryQueuePageTest {
     private static byte[] BYTES_16 = "abcdefghijklmnop".getBytes();
 
     @Test
-    public void testSimpleReadWrite() throws Exception {
+    public void testSingleReadWrite() {
         MemoryQueuePage qp = new MemoryQueuePage(1024);
         int head = qp.write(BYTES_16);
         assertEquals(BYTES_16.length + MemoryQueuePage.OVERHEAD_BYTES, head);
@@ -21,6 +21,30 @@ public class MemoryQueuePageTest {
         assertEquals(1, items.size());
         assertArrayEquals(BYTES_16, items.get(0).getData());
         assertEquals(0, qp.unused());
+    }
+
+    @Test
+    public void testOverflow() {
+        MemoryQueuePage qp = new MemoryQueuePage(15);
+        assertFalse(qp.writable(16));
+        assertEquals(0, qp.write(BYTES_16));
+
+        assertTrue(qp.writable(15 - MemoryQueuePage.OVERHEAD_BYTES));
+        assertFalse(qp.writable(15 - MemoryQueuePage.OVERHEAD_BYTES + 1));
+    }
+
+    @Test
+    public void testDoubleReadWrite() {
+        MemoryQueuePage qp = new MemoryQueuePage(1024);
+        int head = qp.write(BYTES_16);
+        assertEquals(BYTES_16.length + MemoryQueuePage.OVERHEAD_BYTES, head);
+        assertEquals(1, qp.unused());
+
+        head = qp.write(BYTES_16);
+        assertEquals((BYTES_16.length + MemoryQueuePage.OVERHEAD_BYTES) * 2, head);
+        assertEquals(2, qp.unused());
+
+
     }
 
 }
